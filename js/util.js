14f56f0de21672fd3f2f386e2e7eeec6492492f1
jheni1208 <jhenifer.9370323@aluno.mg.gov.br>
2025-10-30 15:55:51 -0300

    arquivo util

---
+++ js/util.js
@@ -0,0 +1,24 @@
+export function mostraMsg(msg, cor = "black", duracao = 500) {
+    //limpar mensagem antiga
+    const msgAntiga = document.querySelectorAll(".msg-sistema");
+    msgAntiga.forEach(m => m.remove());
+
+    const msgDiv = document.createElement("div");
+    msgDiv.classList.add('msg-sistema');
+    msgDiv.textContent = msg;
+    msgDiv.style.color = cor;
+    msgDiv.style.padding = '10px';
+    msgDiv.style.borderRadius = '5px';
+    msgDiv.textAling = "center";
+    msgDiv.style.marginTop = '15px';
+    msgDiv.style.fontWeight = "bold";
+    msgDiv.style.backgroundColor = cor == "red" ? "#fe0e0e"
+        : "#e0ffe0";
+    msgDiv.style.transition = "opacity 0.3 ease";
+    msgDiv.style.opacity = "1";
+    document.appendChild(msgDiv);
+    setTimeout(() => {
+        msgDiv.style.opacity = "0";
+        setTimeout(()=>msgDiv.remove(), 300);
+    }, duracao);
+}
